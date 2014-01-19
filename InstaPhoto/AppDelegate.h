4b60b3b4cc2b7e1c0f82f65ccf0a42f046856b6b
Daniela Baron <baron.daniela@gmail.com>
2014-01-19 13:13:45 -0500

    Initial Commit

---
+++ InstaPhoto/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  InstaPhoto
+//
+//  Created by Daniela Baron on 1/19/2014.
+//  Copyright (c) 2014 DaniB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
