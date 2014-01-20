e9e688ea1184324f3585fe01b43c6c273524662c
Daniela Baron <baron.daniela@gmail.com>
2014-01-20 16:28:05 -0500

    load image from url

--- InstaPhoto/ProfileViewController.m
+++ InstaPhoto/ProfileViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ProfileViewController.h"
+#import "UIImageView+AFNetworking.h"
 
 @interface ProfileViewController ()
 
@@ -28,13 +29,23 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
-    self.view.backgroundColor = [UIColor yellowColor];
-    UIImageView *greggView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"user4"]];
-    //[greggView setContentMode:UIViewContentModeScaleAspectFit];
-    greggView.frame = CGRectMake(20, 20, 100, 114);
-    [self.view addSubview:greggView];
+    UIImageView *profileImageView = [[UIImageView alloc] init];
+    profileImageView.frame = CGRectMake(20, 20, 100, 114);
+    [profileImageView setImageWithURL:[NSURL URLWithString:@"http://animalwelfaretaiwan.webs.com/Cow_with_calf_dsc06514.jpg"]
+                     placeholderImage:[UIImage imageNamed:@"placeholder.png"]];
+    [self.view addSubview:profileImageView];
 }
+
+//- (void)viewDidLoad
+//{
+//    [super viewDidLoad];
+//	// Do any additional setup after loading the view.
+//    self.view.backgroundColor = [UIColor yellowColor];
+//    UIImageView *greggView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"user4"]];
+//    //[greggView setContentMode:UIViewContentModeScaleAspectFit];
+//    greggView.frame = CGRectMake(20, 20, 100, 114);
+//    [self.view addSubview:greggView];
+//}
 
 - (void)didReceiveMemoryWarning
 {
