2e32b8891a655066977c5d383d8e4cbf115970ad
Daniela Baron <baron.daniela@gmail.com>
2014-01-20 12:42:28 -0500

    create cells in table view

--- InstaPhoto/FeedTableViewController.m
+++ InstaPhoto/FeedTableViewController.m
@@ -45,24 +45,26 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-#warning Potentially incomplete method implementation.
-    // Return the number of sections.
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-#warning Incomplete method implementation.
-    // Return the number of rows in the section.
-    return 0;
+    return 50;
 }
 
+// This method runs one time for each row, when it becomes visible
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    static NSString *CellIdentifier = @"Cell";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
-    
-    // Configure the cell...
+    // Check to see if there are any cells of style "Title" we can reuse
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Title"];
+    if (cell == nil) {
+        NSLog(@"Creating new cell");
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"Title"];
+        cell.textLabel.text = @"This is a cell";
+    } else {
+        NSLog(@"Reusing existing cell");
+    }
     
     return cell;
 }
